73cc8520fd6a543c9340b6f7be54d6b172ecfb00
Harrison Bourke <harrisonbourke97@gmail.com>
2021-01-25 10:46:10 +1100

    Aggregate likes function built.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,28 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.aggregateLikes = functions.region("australia-southeast1").firestore
+    .document("exercises/{exerciseId}/likes/{likeId}")
+    .onWrite((change, context) => {
+
+    const exerciseId = context.params.exerciseId;
+    console.log("Exercise ID: ", exerciseId);
+
+    const docRef = admin.firestore().collection("exercises").doc(exerciseId);
+
+    return docRef.collection("likes")
+        .get()
+        .then(querySnapshot => {
+        const likeCount = querySnapshot.size;
+        const lastActivity = admin.database.ServerValue.TIMESTAMP;
+
+        const data = { likeCount, lastActivity };
+
+        return docRef.update(data);
+    })
+    .catch(e => {
+        console.log(e);
+    })
+})
