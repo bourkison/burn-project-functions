01130317135522697accae945dfa749a04364a3a
Harrison Bourke <harrisonbourke97@gmail.com>
2021-01-25 11:28:09 +1100

    Aggregate comments function built

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,12 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-
+// Pulls all likes, counts, then pushes to exercise doc with the value under likeCount.
 exports.aggregateLikes = functions.region("australia-southeast1").firestore
     .document("exercises/{exerciseId}/likes/{likeId}")
     .onWrite((change, context) => {
 
     const exerciseId = context.params.exerciseId;
-    console.log("Exercise ID: ", exerciseId);
 
     const docRef = admin.firestore().collection("exercises").doc(exerciseId);
 
@@ -16,7 +15,7 @@ exports.aggregateLikes = functions.region("australia-southeast1").firestore
         .get()
         .then(querySnapshot => {
         const likeCount = querySnapshot.size;
-        const lastActivity = admin.database.ServerValue.TIMESTAMP;
+        const lastActivity = new Date();
 
         const data = { likeCount, lastActivity };
 
@@ -26,3 +25,37 @@ exports.aggregateLikes = functions.region("australia-southeast1").firestore
         console.log(e);
     })
 })
+
+// Pulls all comments, counts, and saves 5 most recent to commentCount and recentComments.
+exports.aggregateComments = functions.region("australia-southeast1").firestore
+    .document("exercises/{exerciseId}/comments/{commentId}")
+    .onWrite((change, context) => {
+
+    const exerciseId = context.params.exerciseId;
+
+    const docRef = admin.firestore().collection("exercises").doc(exerciseId);
+
+    return docRef.collection("comments").orderBy("createdAt", "desc")
+        .get()
+        .then(querySnapshot => {
+        const commentCount = querySnapshot.size;
+        const lastActivity = new Date();
+
+        const recentComments = [];
+
+        querySnapshot.forEach(doc => {
+            recentComments.push( doc.data() );
+        })
+
+        recentComments.splice(5);
+
+        const data = { recentComments, commentCount, lastActivity }
+
+        return docRef.update(data)
+    })
+    .catch(e => {
+        console.log(e);
+    })
+})
+
+    
